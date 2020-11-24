294087d3320427fdf52749014dc55a4e255097ed
Alan Buss <alanb@localhost.localdomain>
2020-11-23 18:51:47 -0600

    starting to run

--- powerpi/powerreader.py
+++ powerpi/powerreader.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 # power reader daemon
 
-import time
-import json
 
 import serial
 import modbus_tk.defines as cst
@@ -11,12 +9,15 @@ from modbus_tk import modbus_rtu
 import paho.mqtt.client as mqtt
 
 import os
+import time
+import json
+import math
 
 # port configuration adjust as needed
 ports = {
-        'main1': '/dev/ttyUSB0',
+        'main1': '/dev/ttyUSB2',
         'main2': '/dev/ttyUSB1',
-        'solar1': '/dev/ttyUSB2',
+        'solar1': '/dev/ttyUSB0',
         'solar2': '/dev/ttyUSB3'
         }
 
@@ -31,30 +32,10 @@ class PZEMReadError(Exception):
     pass
 
 
-class reading():
-    '''Class to represent energy reading'''
-    def __init__(
-            self,
-            voltage=None,
-            current=None,
-            power=None,
-            energy=None,
-            frequency=None,
-            powerFactor=None,
-            alarm=None
-            ):
-        self.voltage = voltage
-        self.current = current
-        self.power = power
-        self.energy = energy
-        self.frequency = frequency
-        self.powerFactor = powerFactor
-        self.alarm = alarm
-
-
 class pzem():
     """Class to represent PZEM-004t sensor"""
     current = None
+    previous = None
 
     def __init__(self, tty):
         self.master = modbus_rtu.RtuMaster(serial.Serial(
@@ -74,29 +55,30 @@ class pzem():
         except Exception as exp:
             raise PZEMReadError from exp
 
-        voltage = data[0] / 10.0,  # [V]
-        current = (data[1] + (data[2] << 16)) / 1000.0,  # [A]
-        power = (data[3] + (data[4] << 16)) / 10.0,  # [W]
-        energy = data[5] + (data[6] << 16),  # [Wh]
-        frequency = data[7] / 10.0,  # [Hz]
-        powerFactor = data[8] / 100.0,
+        voltage = data[0] / 10.0  # [V]
+        current = (data[1] + (data[2] << 16)) / 1000.0  # [A]
+        power = (data[3] + (data[4] << 16)) / 10.0  # [W]
+        energy = data[5] + (data[6] << 16)  # [Wh]
+        frequency = data[7] / 10.0  # [Hz]
+        powerFactor = data[8] / 100.0
         alarm = data[9]  # 0 = no alarm
 
-        res = reading(
-                voltage=voltage,
-                current=current,
-                power=power,
-                energy=energy,
-                frequency=frequency,
-                powerFactor=powerFactor,
-                alarm=alarm
-                )
+        res = {
+                'voltage': voltage,
+                'current': current,
+                'power': power,
+                'energy': energy,
+                'frequency': frequency,
+                'powerFactor': powerFactor,
+                'alarm': alarm
+                }
+        self.previous = self.current
         self.current = res
         return res
 
 
 class publisher():
-    """Send readings to mqtt broker"""
+    """Send payload to mqtt broker"""
     def __init__(self, broker, user="", password=""):
         self.client = mqtt.Client()
         self.client.username_pw_set(user, password)
@@ -116,12 +98,144 @@ for sensor, port in ports.items():
 
 pub = publisher(broker, user, pw)
 
+class branch():
+    def __init__(self):
+        self.myGen = wattage()
+        self.myNet = wattage()
+        self.myLoad = load() 
 
+    def evaluate(self, main, generation):
+        '''check load and wattages, return true if any changes'''
+        load = self.myLoad.evaluate(main, generation)
+        if not self.myLoad.ready:
+            return False
+
+        gen = self.myGen.add(generation)
+        if load or gen:
+            self.myNet.add(self.myLoad.getValue() - self.myGen.getValue()) # net is positive when pulling from grid, negative if pushing
+            return True
+        return False
+
+    def report(self):
+        return {
+                "net": self.myNet.getValue(),
+                "load": self.myLoad.getValue(),
+                "generation": self.myGen.getValue()
+                }
+
+class wattage():
+    '''class to track wattages read'''
+
+    def __init__(self, limit=10):
+        '''limit is wattage difference that triggers a change'''
+        self.limit = limit
+        self.val = None # last reported value
+
+    def add(self, watts):
+        '''return true if value changed'''
+        if self.val == None or abs(watts - self.val) >= self.limit:
+            self.setValue(watts)
+            return True
+        return False
+
+    def setValue(self, value):
+        self.val = value
+
+    def getValue(self):
+        return self.val
+
+
+class load_evaluator(wattage):
+    '''class for evaluating load by deviation'''
+
+    def __init__(self, limit=10, quantity=3):
+        wattage.__init__(self, limit)
+        self.history = list()
+        self.quantity = 3 # number of values to store in history for standard deviation
+        self.limit = limit
+        self.quantity = quantity
+
+    def add(self, watts):
+        self.history.append(watts)
+        if len(self.history) > self.quantity:
+            self.history.pop(0)
+        self.mean = sum(self.history)/len(self.history)
+        self.deviation()
+        if watts < 0 or len(self.history) < self.quantity:
+            return False
+        over_limit = wattage.add(self, watts)
+        return self.val != None and self.dev < 1
+
+    def mean(self):
+        return self.mean
+
+    def variance(self):
+        return sum((c - self.mean) **2 for c in self.history)/len(self.history)
+
+    def deviation(self):
+        self.dev = math.sqrt(self.variance())
+        return self.dev
+
+    def getDeviation(self):
+        return self.dev
+
+    def current(self):
+        return self.history[-1]
+
+
+class load(wattage):
+    def __init__(self):
+        wattage.__init__(self)
+        self.over = load_evaluator() # load is greater than generation
+        self.under = load_evaluator() # load is less than generation
+        self.ready = False
+
+    def evaluate(self, net, generation):
+        isOver = self.over.add(generation + net) # sum when load is greater than generation
+        isUnder = self.under.add(generation - net) # difference when load is under than generation
+        if isOver or isUnder:
+            self.ready = True
+            if self.over.getDeviation() > self.under.getDeviation():
+                if isUnder:
+                    return self.add(self.under.getValue())
+            else: 
+                if isOver:
+                    return self.add(self.over.getValue())
+        return False
+
+
+class normalizer():
+    '''normalize readings to net, load, generation'''
+    def __init__(self):
+        self.branch1 = branch() 
+        self.branch2 = branch() 
+
+    def normalize(self, readings):
+        b1 = self.branch1.evaluate(
+                    readings['main1']['power'], 
+                    readings['solar1']['power']
+                )
+        b2 = self.branch2.evaluate(
+                    readings['main2']['power'],
+                    readings['solar2']['power']
+                )
+        return b1 or b2 
+
+    def report(self):
+        return {'branch1': self.branch1.report(), 'branch2': self.branch2.report()}
+
+norm = normalizer()
 def reader():
     while 1:
-        res = {'hello': 'al', 'hi': 'laura'}
-        pub.publish(topic, json.dumps(res))
-        time.sleep(5)
+        res = {}
+        try:
+            for sensor in sensors:
+                res[sensor] = sensors[sensor].read()
+            if norm.normalize(res):
+                pub.publish(topic, json.dumps(norm.report()))
+        except Exception as exc:
+            print(exc)
 
+        time.sleep(5)
 
 reader()
